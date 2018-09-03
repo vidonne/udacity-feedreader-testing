10a19b8ea1a5b51a7576ba58bd581da171d6a817
Cedric Vidonne <cedric@vidonne.me>
2018-09-03 20:18:37 +0200

    Corrected Initial Entries and New Feed Selection test to make them more comprehensive. Added some more explanation in the README

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Feedreader testing using Jasmine
 
+[Jasmine](https://jasmine.github.io/) test suite for a web-based application that reads RSS feeds. Project part of the Udacity Nanodegree Front-End Web Developer.
+
 ## Installation
 
 Download/clone to computer, open index.html in your browser, and watch the test results.
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,7 +27,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /*  Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -38,7 +38,7 @@ $(function() {
             }
          });
 
-        /* TODO: Write a test that loops through each feed
+        /*  Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -51,9 +51,9 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /*  Write a new test suite named "The menu" */
     describe('The menu', function() {
-        /* TODO: Write a test that ensures the menu element is
+        /*  Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
@@ -63,7 +63,7 @@ $(function() {
            expect(body.classList.contains('menu-hidden')).toBe(true);
          });
 
-         /* TODO: Write a test that ensures the menu changes
+         /*  Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
@@ -79,9 +79,9 @@ $(function() {
 
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /*  Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
-        /* TODO: Write a test that ensures when the loadFeed
+        /*  Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -92,32 +92,32 @@ $(function() {
          });
 
          it('completes its work', function() {
-           const feed = document.querySelector('.feed');
-           expect(feed.children.length > 0).toBe(true);
+           const feed = document.querySelectorAll('.feed .entry');
+           expect(feed.length > 0).toBe(true);
          });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /*  Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function() {
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /*  Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
          const feed = document.querySelector('.feed');
-         const firstFeed = [];
+         let oldFeed;
+         let newFeed;
 
          beforeEach(function(done) {
-           loadFeed(0);
-           Array.from(feed.children).forEach(function(entry) {
-             firstFeed.push(entry.innerText);
+           loadFeed(0, function() {
+             oldFeed = feed.innerHTML;
+             loadFeed(1, function() {
+               newFeed = feed.innerHTML;
+             });
            });
-           loadFeed(1, done);
          });
 
          it('content changes', function() {
-           Array.from(feed.children).forEach(function(entry, index) {
-             expect(entry.innerText === firstFeed[index]).toBe(false);
-           });
+           expect(oldFeed).not.toBe(newFeed);
          });
 
     });
